4d7df8af7b91f0bb0b595ccf382f114a8f03ea92
CPrasa <chathuryaprasad@gmail.com>
2025-06-04 14:04:13 +0530

    Add Download and install Hadoop to Hadoop installation script

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -28,3 +28,10 @@ echo "Generating SSH keys..."
 sudo -u hadoop ssh-keygen -t rsa -P "" -f /home/hadoop/.ssh/id_rsa
 sudo -u hadoop cat /home/hadoop/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys
 sudo -u hadoop chmod 0600 /home/hadoop/.ssh/authorized_keys
+
+# Download and install Hadoop
+echo "Downloading Hadoop..."
+wget https://downloads.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
+tar xzf hadoop-3.3.6.tar.gz
+sudo mv hadoop-3.3.6 /usr/local/hadoop
+sudo chown -R hadoop:hadoop /usr/local/hadoop
